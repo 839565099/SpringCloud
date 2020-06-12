e80e2ea44e198bf588d9be044242f3ccd40f07a1
张翼麒 <839565099@qq.com>
2020-06-12 23:39:24 +0800

    cloud学习

---
+++ springcloud-payment8004/src/main/java/com/zyq/com/zyq/PaymentController.java
@@ -0,0 +1,27 @@
+package com.zyq.com.zyq;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * 作者：张翼麒
+ * Date:2020-6-12
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/zk")
+    public String paymentzk(){
+        log.info("~~~~~~~~~~~~："+serverPort);
+        return "springcloud with zookeeper:"+serverPort+"\t"+ UUID.randomUUID().toString();
+
+    }
+
+}
