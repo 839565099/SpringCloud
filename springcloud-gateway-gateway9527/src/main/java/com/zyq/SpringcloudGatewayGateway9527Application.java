4c994e0ce1d8219b82cfb61ab8996158ffc2af9e
zhangyiqi <>
2020-06-24 23:32:38 +0800

    springcloud学习

---
+++ springcloud-gateway-gateway9527/src/main/java/com/zyq/SpringcloudGatewayGateway9527Application.java
@@ -0,0 +1,15 @@
+package com.zyq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringcloudGatewayGateway9527Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudGatewayGateway9527Application.class, args);
+    }
+
+}
---
+++ springcloud-gateway-gateway9527/src/main/java/com/zyq/filter/MyLogGateWayFilter.java
@@ -0,0 +1,33 @@
+package com.zyq.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.lang.annotation.Annotation;
+
+@Component
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (uname == null) {
+            System.out.println("uname为空，非法");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
