a5a9a0083ab768f827cc4d25bec4975280ae62fe
zhangyiqi <>
2020-06-27 22:55:25 +0800

    springcloud学习

---
+++ springcloud-config-client3366/src/main/java/com/zyq/SpringcloudConfigClient3366Application.java
@@ -0,0 +1,15 @@
+package com.zyq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringcloudConfigClient3366Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConfigClient3366Application.class, args);
+    }
+
+}
